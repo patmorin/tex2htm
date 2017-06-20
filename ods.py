44d17bdec071ffc91ed245462a01ff9bd911dc86
Pat Morin <morin@scs.carleton.ca>
2017-06-20 15:20:28 -0400

    Got Java method imports working

--- ods.py
+++ ods.py
@@ -2,6 +2,11 @@
 import os
 import re
 
+from pygments import highlight
+from pygments.lexers import JavaLexer
+from pygments.formatters import HtmlFormatter
+
+
 from catlist import catlist
 #catlist = list
 
@@ -34,20 +39,43 @@ def setup_command_handlers(command_handlers):
 def get_member(member, clz):
     basedir = '/home/morin/ods/java'
     filename = basedir+os.path.sep+clz+'.java' # FIXME: hard-coded
-
-    for line in open(filename).read().splitlines()
-        line = re.sub('public|static|void|protected', '', line)
-
+    code = catlist()
+    d = 0
+    writing = False
+    for line in open(filename).read().splitlines():
+        line = re.sub('(static|public|protected|private|final)\s+', '', line)
+        line = re.sub('\t', '    ', line)
+        #print("{}: {}".format(d, line))
+        if d == 1:
+            # Perl: /^\s*($k\s+)*(\<[^>]\>)?\s*[$w]+\s+([$w]+)\(.*\)/
+            m = re.match('\s*(<[^>]*>)?\s*\w+\s*(\w+)\s*\((.*)\)\s*{\s*$', line)
+            if m:
+                name = m.group(2)
+                args = [x.strip() for x in m.group(3).split(',') if x]
+                argnames = [x.split()[-1] for x in args]
+                sig = '{}({})'.format(name, ",".join(argnames))
+                if sig == member:
+                    writing = True
+                #print(m.group(0))
+                #print("name='{}', args={} argnames={}".format(name, args, argnames))
+        d += line.count('{')
+        d -= line.count('}')
+        if writing:
+            if not re.search('IndexOutOfBoundsException', line):
+                code.append(line)
+            if d <= 1:
+                writing = False
+    return code
 
 def process_codeimport_cmd(tex, cmd, mode):
     blocks = catlist(['<div class="codeimport">'])
-    blocks.extend(tex2htm.process_recursively(cmd.args[0], mode))
-    blocks.append("</div><!-- codeimport -->")
-    print(cmd)
+    # blocks.extend(tex2htm.process_recursively(cmd.args[0], mode))
     clz, members = cmd.args[0].split('.', 1)
     members = members.split('.')
     for member in members:
-        get_member(member, clz)
+        code = get_member(member, clz)
+        blocks.append(highlight("\n".join(code), JavaLexer(), HtmlFormatter()))
+    blocks.append("</div><!-- codeimport -->")
     return blocks
 
 def setup_environment_handlers(environment_handlers):
