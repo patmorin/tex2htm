3508988e16b650e918b5895d1e132fedd0ec1860
Pat Morin <morin@scs.carleton.ca>
2017-06-20 16:06:31 -0400

    Support for importing instance variables

--- ods.py
+++ ods.py
@@ -1,5 +1,6 @@
 """ Open Data Structures specific extension for tex2htm """
 import os
+import sys
 import re
 
 from pygments import highlight
@@ -32,32 +33,44 @@ def setup_command_handlers(command_handlers):
     command_handlers['codeimport'] =  process_codeimport_cmd
     command_handlers['javaimport'] =  process_codeimport_cmd
     command_handlers['etal'] = lambda text, cmd, mode: catlist(["<em>et al</em>"])
+    command_handlers['lang'] = lambda text, cmd, mode: catlist(["Java"])
+    worthless = ['cpponly']
+    for c in worthless:
+        command_handlers[c] = tex2htm.process_cmd_worthless
     strip = ['javaonly', 'notpcode']
     for c in strip:
         command_handlers[c] = tex2htm.process_cmd_strip
 
 def get_member(member, clz):
-    basedir = '/home/morin/ods/java'
+    print(member)
+    basedir = os.path.dirname(sys.argv[1]) + os.path.sep \
+                + '..' + os.path.sep + 'java'
     filename = basedir+os.path.sep+clz+'.java' # FIXME: hard-coded
     code = catlist()
     d = 0
     writing = False
+    found = False
     for line in open(filename).read().splitlines():
         line = re.sub('(static|public|protected|private|final)\s+', '', line)
         line = re.sub('\t', '    ', line)
-        #print("{}: {}".format(d, line))
         if d == 1:
-            # Perl: /^\s*($k\s+)*(\<[^>]\>)?\s*[$w]+\s+([$w]+)\(.*\)/
             m = re.match('\s*(<[^>]*>)?\s*\w+\s*(\w+)\s*\((.*)\)\s*{\s*$', line)
             if m:
+                # this line is a method definition
+                found = True
                 name = m.group(2)
                 args = [x.strip() for x in m.group(3).split(',') if x]
                 argnames = [x.split()[-1] for x in args]
                 sig = '{}({})'.format(name, ",".join(argnames))
                 if sig == member:
                     writing = True
-                #print(m.group(0))
-                #print("name='{}', args={} argnames={}".format(name, args, argnames))
+            m = re.match('\s*(<[^>]*>)?\s*\w+\s*(\w+)\s*;\s*$', line)
+            if m:
+                # this is an instance variable
+                name = m.group(2)
+                if name == member:
+                    code.append(line)
+
         d += line.count('{')
         d -= line.count('}')
         if writing:
@@ -65,6 +78,10 @@ def get_member(member, clz):
                 code.append(line)
             if d <= 1:
                 writing = False
+    if not found:
+        msg = 'ERROR: {}.{} not found'.format(clz, member)
+        tex2htm.warn(msg)
+        code.append('// {}'.format(msg))
     return code
 
 def process_codeimport_cmd(tex, cmd, mode):
@@ -72,9 +89,10 @@ def process_codeimport_cmd(tex, cmd, mode):
     # blocks.extend(tex2htm.process_recursively(cmd.args[0], mode))
     clz, members = cmd.args[0].split('.', 1)
     members = members.split('.')
+    code = catlist()
     for member in members:
-        code = get_member(member, clz)
-        blocks.append(highlight("\n".join(code), JavaLexer(), HtmlFormatter()))
+        code.extend(get_member(member, clz))
+    blocks.append(highlight("\n".join(code), JavaLexer(), HtmlFormatter()))
     blocks.append("</div><!-- codeimport -->")
     return blocks
 
